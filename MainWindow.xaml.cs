fee3345f3cfc324809791456672a9f7dc466e69e
Eldrven <kszewc97@gmail.com>
2021-09-21 00:04:36 +0200

    Added data entities and initial XAML for dashboard

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,38 @@ using System.Windows.Shapes;
 
 namespace ClientX
 {
+    
+
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DatabaseEntities context = new DatabaseEntities();
+        CollectionViewSource custViewSource;
+        CollectionViewSource ordViewSource;
+
         public MainWindow()
         {
             InitializeComponent();
+            custViewSource = (CollectionViewSource)FindResource("customersViewSource");
+            ordViewSource = (CollectionViewSource)FindResource("customersOrdersViewSource");
+            DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Load is an extension method on IQueryable,    
+            // defined in the System.Data.Entity namespace.   
+            // This method enumerates the results of the query,    
+            // similar to ToList but without creating a list.   
+            // When used with Linq to Entities, this method    
+            // creates entity objects and adds them to the context.   
+            context.Customers.Load();
+
+            // After the data is loaded, call the DbSet<T>.Local property    
+            // to use the DbSet<T> as a binding source.   
+            custViewSource.Source = context.Customers.Local;
         }
     }
 }
